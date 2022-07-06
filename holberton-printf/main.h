9fa1ec4a185eb4fc3d74bb4e1811fb93cc516ed6
armigjoka <armigjoka090@gmail.com>
2022-07-06 03:32:31 -0700

    main.h main.c _putchar.c

---
+++ holberton-printf/main.h
@@ -0,0 +1,23 @@
+#ifndef _printf_
+#define _printf_
+
+/**
+  * struct print - structure to print
+  * @print: printed characters
+  * @p: pointer
+  */
+typedef struct print
+{
+	char *print;
+	int (*p)();
+};
+int_putchar(char c)
+
+int_printf(const char *format, ...)
+
+
+int_print_char(va_list);
+int_print_string(var_list arg);
+int_print_int(var_list arg);
+int_print_float(var_list arg);
+#endif
