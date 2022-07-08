8a180209624dbcf07c89e572bf02288b05bc9cd0
armigjoka <armigjoka090@gmail.com>
2022-07-08 03:35:55 -0700

    task 0

--- cover_2_func.c
+++ cover_2_func.c
@@ -42,34 +42,3 @@ int _print_hex_u(va_list project)
 		count += _putchar(s[i]);
 	return (count);
 }
-
-/**
- * _print_rot13 - prints the hexadecimal long
- * @project: pointer where to get action over
- * Return: number of the counter
- */
-
-/* int _print_rot13(va_list project)*/
-/**
-{
-	unsigned int n;
-	int i;
-	char *s;
-	int count = 0;
-
-	n = va_arg(project, unsigned int);
-	s = convert(n, 16);
-
-	if (!n)
-		count += _putchar('0');
-	for (i = 0; s[i] && n; i++)
-	{
-		if (s[i] >= 'A' && s[i] <= 'F')
-			count += _putchar(s[i] + ' ');
-		else
-			count += _putchar(s[i]);
-	}
-	return (count);
-
-}
-*/
